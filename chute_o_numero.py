c0bd09c11b3c866921b0190823320fffbfe16ee4
unknown <izabellasilveir@gmail.com>
2020-10-02 11:24:46 -0300

    2 projeto

---
+++ chute_o_numero.py
@@ -0,0 +1,12 @@
+import random
+
+class ChuteONumero:
+    def __init__(self):
+        self.valor_aleatorio = 0
+        self.valor_maximo
+        self.valor_minimo
+    
+    def Iniciar(self):
+        self.GerarNumeroAleatorio()
+
+    def GerarNumeroAleatorio(self):
